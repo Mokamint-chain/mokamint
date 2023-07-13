2d8feeb28efb00abeca402ea786716f32ef93e82
Fausto Spoto <fausto.spoto@gmail.com>
2023-07-13 11:10:10 +0200

    mokamint-node peers ls reports extra information about the peers now

--- io-mokamint-node-tools/src/main/java/io/mokamint/node/tools/internal/peers/List.java
+++ io-mokamint-node-tools/src/main/java/io/mokamint/node/tools/internal/peers/List.java
@@ -19,7 +19,8 @@ package io.mokamint.node.tools.internal.peers;
 import static io.hotmoka.exceptions.CheckSupplier.check;
 import static io.hotmoka.exceptions.UncheckFunction.uncheck;
 
-import java.util.OptionalInt;
+import java.io.IOException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,19 +30,33 @@ import java.util.stream.Stream;
 import io.mokamint.node.PeerInfos;
 import io.mokamint.node.api.PeerInfo;
 import io.mokamint.node.remote.RemotePublicNode;
+import io.mokamint.node.remote.RemotePublicNodes;
 import io.mokamint.node.tools.internal.AbstractPublicRpcCommand;
+import jakarta.websocket.DeploymentException;
 import jakarta.websocket.EncodeException;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Help.Ansi;
+import picocli.CommandLine.Option;
 
 @Command(name = "ls", description = "List the peers of a node.")
 public class List extends AbstractPublicRpcCommand {
 
+	@Option(names = "--verbose", description = "report extra information about the peers", defaultValue = "false")
+	private boolean verbose;
+
 	private final static Logger LOGGER = Logger.getLogger(List.class.getName());
+	
+	/**
+	 * The maximal length of a peer URI. Beyond that number of characters, the URI string gets truncated.
+	 */
+	private final static int MAX_PEER_LENGTH = 50;
 
 	private void body(RemotePublicNode remote) throws TimeoutException, InterruptedException {
 		try {
 			PeerInfo[] infos = remote.getPeers().sorted().toArray(PeerInfo[]::new);
+			if (infos.length == 0)
+				return;
+
 			if (json()) {
 				var encoder = new PeerInfos.Encoder();
 				System.out.println(check(EncodeException.class, () ->
@@ -49,10 +64,17 @@ public class List extends AbstractPublicRpcCommand {
 				));
 			}
 			else {
-				OptionalInt maxLength = Stream.of(infos).mapToInt(info -> info.getPeer().toString().length()).max();
-				int size = maxLength.isEmpty() || maxLength.getAsInt() > 50 ? 50: maxLength.getAsInt();
-				printLine("URI", size, "points", "status", true);
-				Stream.of(infos).forEachOrdered(info -> printLine(info.getPeer().toString(), size, String.valueOf(info.getPoints()), info.isConnected() ? "connected" : "disconnected", false));
+				int maxLength = Math.min(MAX_PEER_LENGTH, Stream.of(infos).mapToInt(info -> info.getPeer().toString().length()).max().getAsInt());
+				if (verbose)
+					System.out.println(Ansi.AUTO.string("@|green " + formatLine("URI", maxLength, "points", "status", "UUID", "version") + "|@"));
+				else
+					System.out.println(Ansi.AUTO.string("@|green " + formatLine("URI", maxLength, "points", "status") + "|@"));
+
+				try (var pool = new ForkJoinPool()) {
+					pool.execute(() ->
+						Stream.of(infos).parallel().map(info -> formatLine(info, maxLength)).forEachOrdered(System.out::println)
+					);
+				}
 			}
 		}
 		catch (EncodeException e) {
@@ -61,12 +83,32 @@ public class List extends AbstractPublicRpcCommand {
 		}
 	}
 
-	private void printLine(String peer, int peerNameSize, String points, String connected, boolean color) {
-		String line = String.format("%-" + peerNameSize + "s   %6s  %s", peer, points, connected);
-		if (color)
-			line = Ansi.AUTO.string("@|red " + line + "|@");
+	private String formatLine(PeerInfo info, int maxLength) {
+		if (verbose) {
+			String version = "<unknown>";
+			String uuid = "<unknown>";
 
-		System.out.println(line);
+			try (var remote = RemotePublicNodes.of(info.getPeer().getURI(), 10000)) {
+				var peerInfo = remote.getInfo();
+				version = peerInfo.getVersion().toString();
+				uuid = peerInfo.getUUID().toString();
+			}
+			catch (IOException | DeploymentException | TimeoutException | InterruptedException e) {
+				LOGGER.log(Level.WARNING, "cannot contact " + info.getPeer(), e);
+			}
+
+			return formatLine(info.getPeer().toString(), maxLength, String.valueOf(info.getPoints()), info.isConnected() ? "connected" : "disconnected", uuid, version);
+		}
+		else
+			return formatLine(info.getPeer().toString(), maxLength, String.valueOf(info.getPoints()), info.isConnected() ? "connected" : "disconnected");
+	}
+
+	private String formatLine(String peer, int peerNameSize, String points, String connected) {
+		return String.format("%-" + peerNameSize + "s   %6s  %-12s", peer, points, connected);
+	}
+
+	private String formatLine(String peer, int peerNameSize, String points, String connected, String uuid, String version) {
+		return String.format("%-" + peerNameSize + "s   %6s  %-12s  %-36s  %s", peer, points, connected, uuid, version);
 	}
 
 	@Override
