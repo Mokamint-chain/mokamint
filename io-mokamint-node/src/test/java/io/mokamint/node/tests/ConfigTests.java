dbf287c415bbca2efe96e22b36d450579d590148
Fausto Spoto <fausto.spoto@gmail.com>
2023-08-26 09:03:59 +0200

    Added test

--- io-mokamint-node/src/test/java/io/mokamint/node/tests/ConfigTests.java
+++ io-mokamint-node/src/test/java/io/mokamint/node/tests/ConfigTests.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.LogManager;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import io.mokamint.node.ConsensusConfigs;
 import jakarta.websocket.DecodeException;
@@ -22,6 +26,16 @@ public class ConfigTests {
 		var config1 = ConsensusConfigs.defaults().build();
 		String encoded = new ConsensusConfigs.Encoder().encode(config1);
 		var config2 = new ConsensusConfigs.Decoder().decode(encoded);
+		assertEquals(config1, config2);
+	}
+
+	@Test
+	@DisplayName("configs are correctly dumped into TOML and reloaded from TOML")
+	public void dumpLoadTOMLWorks(@TempDir Path dir) throws EncodeException, DecodeException, NoSuchAlgorithmException, IOException {
+		var path = dir.resolve("config.toml");
+		var config1 = ConsensusConfigs.defaults().build();
+		Files.writeString(path, config1.toToml(), StandardCharsets.UTF_8);
+		var config2 = ConsensusConfigs.load(path).build();
 		assertEquals(config1, config2);
 	}
 
