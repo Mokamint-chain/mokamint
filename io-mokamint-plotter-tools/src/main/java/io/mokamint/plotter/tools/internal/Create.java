86593ee67dd0a10b51df50a7f7300f075e88f014
Fausto Spoto <fausto.spoto@gmail.com>
2023-09-07 12:35:55 +0200

    Plot creation allows one to specify the keys of the node and of the plot

--- io-mokamint-plotter-tools/src/main/java/io/mokamint/plotter/tools/internal/Create.java
+++ io-mokamint-plotter-tools/src/main/java/io/mokamint/plotter/tools/internal/Create.java
@@ -16,13 +16,18 @@ limitations under the License.
 
 package io.mokamint.plotter.tools.internal;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.function.Function;
 
+import io.hotmoka.crypto.Base58;
 import io.hotmoka.crypto.HashingAlgorithms;
+import io.hotmoka.crypto.SignatureAlgorithms;
 import io.mokamint.plotter.Plots;
 import io.mokamint.tools.AbstractCommand;
 import io.mokamint.tools.CommandException;
@@ -45,6 +50,18 @@ public class Create extends AbstractCommand {
 	@Parameters(index = "2", description = "the amount of nonces")
 	private long length;
 
+	@Parameters(index = "3", description = "the chain identifier of the network for which the plot will be used")
+	private String chainId;
+
+	@Parameters(index = "4", description = "the base58-encoded public key of the node for which the plot will be used")
+	private String nodePublicKeyBase58;
+
+	@Parameters(index = "5", description = "the base58-encoded public key of the plot")
+	private String plotPublicKeyBase58;
+
+	@Option(names = "--extra", description = "application-specific base58-encoded extra data for the plot", defaultValue = "")
+	private String extraBase58;
+
 	@Option(names = "--hashing", description = "the name of the hashing algorithm for the nonces", defaultValue = "shabal256")
 	private String hashing;
 
@@ -57,7 +74,14 @@ public class Create extends AbstractCommand {
 			throw new CommandException("Failed to overwrite \"" + path + "\"!", e);
 		}
 
-		var prolog = new byte[] { 11, 13, 24, 88 };
+		byte[] prolog;
+		
+		try {
+			prolog = computeProlog();
+		}
+		catch (IOException e) {
+			throw new CommandException("Cannot compute the prolog of the plot!", e);
+		}
 
 		try (var plot = Plots.create(path, prolog, start, length, HashingAlgorithms.of(hashing, Function.identity()), this::onNewPercent)) {
 		}
@@ -76,5 +100,48 @@ public class Create extends AbstractCommand {
 			System.out.print(Ansi.AUTO.string("@|bold,red " + percent + "%|@ "));
 		else
 			System.out.print(percent + "% ");
+	}
+
+	private byte[] computeProlog() throws IOException {
+		try (var os = new ByteArrayOutputStream()) {
+			var chainIdBytes = chainId.getBytes(Charset.forName("UTF-8"));
+			os.write(chainIdBytes.length);
+			os.write(chainIdBytes);
+			os.write(getKeyAsBytes(nodePublicKeyBase58));
+			os.write(getKeyAsBytes(plotPublicKeyBase58));
+			var extraBytes = bytesFromBase58(extraBase58);
+			os.write(extraBytes.length);
+			os.write(extraBytes);
+	
+			return os.toByteArray();
+		}
+	}
+
+	private byte[] bytesFromBase58(String base58) {
+		try {
+			return Base58.decode(base58);
+		}
+		catch (IllegalArgumentException e) {
+			throw new CommandException("The string " + base58 + " is not in Base58 format!", e);
+		}
+	}
+
+	private byte[] getKeyAsBytes(String base58EncodedPublicKey) {
+		byte[] publicKeyBytes = bytesFromBase58(base58EncodedPublicKey);
+	
+		if (publicKeyBytes.length != 32)
+			throw new CommandException("The public key " + base58EncodedPublicKey + " is not 32 bytes long!");
+	
+		try {
+			SignatureAlgorithms.ed25519(Function.identity()).publicKeyFromEncoding(publicKeyBytes);
+		}
+		catch (NoSuchAlgorithmException e) {
+			throw new CommandException("The ed25519 signature algorithm is not available!", e);
+		}
+		catch (InvalidKeySpecException e) {
+			throw new CommandException("The public key " + base58EncodedPublicKey + " is not a legal ed25519 public key!", e);
+		}
+	
+		return publicKeyBytes;
 	}
 }
