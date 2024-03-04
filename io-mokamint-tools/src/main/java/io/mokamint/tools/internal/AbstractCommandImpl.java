1381a993f8490a926e4a4a47a5b66f9f1cbc994e
Fausto Spoto <fausto.spoto@gmail.com>
2024-03-04 22:12:26 +0100

    Removed unused methods

--- io-mokamint-tools/src/main/java/io/mokamint/tools/internal/AbstractCommandImpl.java
+++ io-mokamint-tools/src/main/java/io/mokamint/tools/internal/AbstractCommandImpl.java
@@ -45,52 +45,6 @@ public abstract class AbstractCommandImpl implements Callable<Void> {
 	}
 
 	/**
-	 * Centers the given string on the given number of columns.
-	 * 
-	 * @param s the string to center
-	 * @param size the number of columns
-	 * @return the centered string {@code s}, with spaces before and after if needed
-	 */
-	protected static String center(String s, int size) {
-		if (size <= s.length())
-			return s;
-
-		int spaceSize = size - s.length();
-		int prefixSize = spaceSize / 2;
-		int suffixSize = (spaceSize + 1) / 2;
-
-		return " ".repeat(prefixSize) + s + " ".repeat(suffixSize);
-	}
-
-	/**
-	 * Aligns the given string on the right of the given number of columns.
-	 * 
-	 * @param s the string to center
-	 * @param size the number of columns
-	 * @return the right-aligned string {@code s}, with spaces before if needed
-	 */
-	protected static String rightAlign(String s, int size) {
-		if (size <= s.length())
-			return s;
-		else
-			return " ".repeat(size - s.length()) + s;
-	}
-
-	/**
-	 * Aligns the given string on the left of the given number of columns.
-	 * 
-	 * @param s the string to center
-	 * @param size the number of columns
-	 * @return the left-aligned string {@code s}, with spaces after if needed
-	 */
-	protected static String leftAlign(String s, int size) {
-		if (size <= s.length())
-			return s;
-		else
-			return s + " ".repeat(size - s.length());
-	}
-
-	/**
 	 * Executes the command.
 	 * 
 	 * @throws CommandException if something erroneous must be logged and the user must be informed
